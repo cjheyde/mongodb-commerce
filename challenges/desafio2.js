3e05f946fe29856bf96cbd66d268851cbb0160a6
cjheyde <carlaheyde@gmail.com>
2022-10-17 17:53:48 -0300

    feat: :white_check_mark: challenge 2 - desafio2 ok

---
+++ challenges/desafio2.js
@@ -0,0 +1,4 @@
+// 2 - Ordene a coleção produtos pela quantidade de lanches vendidos em ordem crescente, mostrando apenas o nome e a quantidade de lanches vendidos
+db.produtos.find(
+  {}, { nome: 1, vendidos: 1, _id: 0 },
+  ).sort({ vendidos: 1 });
