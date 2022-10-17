1a449573450f0737251725fb53dce1225d87a126
cjheyde <carlaheyde@gmail.com>
2022-10-17 18:29:40 -0300

    feat: :white_check_mark: cahllenge4 - desafio 4 ok

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,6 @@
 // 4 - Retorne os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
+db.produtos.find(
+  {
+    vendidos: { $gt: 50, $lt: 100 },
+  }, { nome: 1, vendidos: 1, _id: 0 },
+).sort({ vendidos: 1 });
