a35be887f9e70adec859be711331dba858514491
cjheyde <carlaheyde@gmail.com>
2022-10-17 18:47:43 -0300

    feat: :white_check_mark: challenge 3 - desafio 3 ok

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,6 @@
 // 3 - Retorne o lanche mais vendido, mostrando apenas o nome e a quantidade do lanche mais vendido
+db.produtos.find(
+  {
+    
+  }, { nome: 1, vendidos: 1, _id: 0 },
+).sort({ vendidos: -1 }).limit(1);
