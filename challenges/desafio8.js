881d4e0c0957ff85d8ffd7af819800d191544593
cjheyde <carlaheyde@gmail.com>
2022-10-18 01:35:11 -0300

    feat: :white_check_mark: challenge8 - desafio 8 ok

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,1 +1,8 @@
 // 8 - Delete os lanches com menos de 50 curtidas e retorne o nome dos lanches que restaram no banco
+db.produtos.deleteMany({
+  curtidas: {
+    $lt: 50,
+  },
+});
+
+db.produtos.find({}, { nome: 1, _id: 0 });
