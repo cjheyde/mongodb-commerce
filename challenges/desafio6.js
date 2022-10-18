2139e6909753ad29cdae4a4cf912a03fe20f1f38
cjheyde <carlaheyde@gmail.com>
2022-10-18 01:08:48 -0300

    feat: :white_check_mark: challenge 6 - Desafio 6 ok

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,10 @@
 // 6 - Retorne o nome e as curtidas dos lanches que tiveram curtidas maiores que 10 e menores que 100
+db.produtos.find(
+  {
+    $and: [
+      { curtidas: { $gt: 10 } },
+      { curtidas: { $lt: 100 } },
+    ],
+  },
+  { _id: 0, nome: 1, curtidas: 1 },
+);
