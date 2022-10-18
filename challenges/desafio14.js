13886b564cdfb98587524e433e0a01bc7e259df3
cjheyde <carlaheyde@gmail.com>
2022-10-17 23:25:39 -0300

    feat: :white_check_mark: challenge14 - desafio 14 ok

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,7 @@
 // 14 - Crie uma query que retorne todos os lanches que possuem picles em seus ingredientes e mostre apenas os 3 primeiros itens contidos no array valoresNutricionais
+db.produtos.find(
+  {
+    ingredientes: { $eq: "picles" },
+  },
+  { nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 }, _id: 0 },
+);
