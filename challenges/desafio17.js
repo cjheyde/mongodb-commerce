834865eac443f6fa5483f74ae13176b192fb6136
cjheyde <carlaheyde@gmail.com>
2022-10-17 23:52:19 -0300

    feat: :white_check_mark: challenge17 - Desafio 17 ok

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,12 @@
 // 17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
+// Crie uma query que insira na coleção resumoProdutos um documento com os campos: franquia com o valor McDonalds e totalProdutos com o valor sendo a quantidade total de produtos registrados na coleção produtos.
+
+// Crie uma query que retorne os campos franquia e o totalProdutos da coleção resumoProdutos, resultantes da primeira query.
+db.resumoProdutos.insertOne(
+  {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.countDocuments({}),
+  },
+);
+
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
