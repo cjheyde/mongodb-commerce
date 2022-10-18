16aa276b58d2cc6ba25afdf07a57d7a9ed84b56e
cjheyde <carlaheyde@gmail.com>
2022-10-18 01:06:10 -0300

    feat: :white_check_mark: challenge 5 -> Desafio 5 ok

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,1 +1,10 @@
 // 5 - Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: { $eq: 36 } },
+      { vendidos: { $eq: 85 } },
+    ],
+  },
+  { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
+);
