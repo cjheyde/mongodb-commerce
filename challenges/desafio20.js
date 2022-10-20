1c170fff5c3acc101c25e050bc0814543153e3c1
cjheyde <carlaheyde@gmail.com>
2022-10-19 21:28:48 -0300

    feat: :white_check_mark: challenge20 - desafio 20 ok

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,14 @@
-// 20 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo
+// 20 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijodb.produtos.updateMany
+// Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  {
+    $pop: {
+      ingredientes: -1,
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
