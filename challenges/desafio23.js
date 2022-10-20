ca6efa0c2ec691af134c25edba5eafe697b3d317
cjheyde <carlaheyde@gmail.com>
2022-10-19 21:41:58 -0300

    feat: :white_check_mark: challenge23, desafio 23 ok

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,21 @@
 // 23 - Insira os valores combo e tasty no array tags de todos os sanduíches e aproveite para deixar os valores em ordem alfabética ascendente (A a Z)
+
+// Crie uma query que faça tanto a inserção dos valores combo e tasty no array tags de todos os sanduíches. Ordene os valores de tags em ordem alfabética ascendente.
+
+// Crie uma query que retorne o nome e tags de todos os documentos.
+
+db.produtos.updateMany(
+  {},
+  {
+    $push:
+    {
+      tags:
+      {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
