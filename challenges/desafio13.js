dce851c86dcafc1e1e978c38e8e0e1cff2a06f8b
cjheyde <carlaheyde@gmail.com>
2022-10-19 21:06:53 -0300

    feat: :bug: Challenge 13 - bug fixed

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,6 +1,5 @@
 // 13 - Inclua o campo criadoPor em todos os documentos, colocando Ronald McDonald no valor desse campo
-db.produtos.updateMany({}, 
-  { $set: { criadoPor: "Ronald McDonald" } },
-);
+db.produtos.updateMany({},
+  { $set: { criadoPor: "Ronald McDonald" } });
 
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
