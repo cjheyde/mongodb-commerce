49defb1df5013a33c8cf94d09d7d7004897b00c1
cjheyde <carlaheyde@gmail.com>
2022-10-20 01:09:45 -0300

    feat: :white_check_mark: challenge16 - desafio 16 ok

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,1 +1,10 @@
 // 16 - Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
+// Crie uma query que inclua somente ao sanduíche Big Mac o campo ultimaModificacao com a data corrente. Para a data corrente faça uso do tipo date ou timestamp.
+
+// Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: Date() } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
