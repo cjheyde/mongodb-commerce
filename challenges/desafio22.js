6c15a8dd3c064364da875faf691650562a4b726a
cjheyde <carlaheyde@gmail.com>
2022-10-19 21:58:13 -0300

    feat: :white_check_mark: challenge24 - desafio24 ok

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,38 @@
 // 22 - Adicione a quantidade de vendas dos sanduíches por dia da semana
+// Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
+
+// Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino aos sábados em 120.
+
+// Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
+
+db.produtos.updateMany(
+  {},
+  {
+    $set:
+    {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $inc:
+    {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateOne(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  {
+    $inc:
+    {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
