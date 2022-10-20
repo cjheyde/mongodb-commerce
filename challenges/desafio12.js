19e064a118afc1c7668524de9b05941e2408708b
cjheyde <carlaheyde@gmail.com>
2022-10-19 21:01:37 -0300

    feat: :white_check_mark: challenge12 desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,7 @@
 // 12 - Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
