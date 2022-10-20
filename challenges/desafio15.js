2424f12d60e01d8699aa01d0f83f85d2a4ac21ff
cjheyde <carlaheyde@gmail.com>
2022-10-20 00:51:59 -0300

    feat: :white_check_mark: Challenge15 - desafio 15 ok

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,1 +1,17 @@
 // 15 - Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: 0 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "ave" } } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
