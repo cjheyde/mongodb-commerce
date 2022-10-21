784a0e1dd009f3571f8b70e2d6becdaa0d608a1e
cjheyde <carlaheyde@gmail.com>
2022-10-21 11:29:15 -0300

    feat: :white_check_mark: Challenge29 - desafio 29 ok

--- challenges/desafio29.js
+++ challenges/desafio29.js
@@ -1,1 +1,14 @@
 // 29 - Renomeie o campo descricao para descricaoSite em todos os documentos
+// Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+
+// Crie uma query que retorne o nome e descricaoSite de todos os documentos.
+db.produtos.updateMany(
+  { },
+  {
+    $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, descricaoSite: 1 });
