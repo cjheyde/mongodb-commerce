5d78e76efafc27b6d36dd8eec433b8021099922e
cjheyde <carlaheyde@gmail.com>
2022-10-21 11:33:22 -0300

    feat: :white_check_mark: Challenge30 - desafio 30 ok

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,1 +1,14 @@
 // 30 - Remova o campo curtidas do item Big Mac
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+// Crie uma query que retorne o nome para todos os documentos e curtidas (exceto para Big Mac).
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $unset: {
+      curtidas: { $exists: true },
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
